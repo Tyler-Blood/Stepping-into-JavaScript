1283c071fc87844a261d2bebdca7f7c136139dbf
TylerBlood <tylerlblood@gmail.com>
2019-02-14 17:06:38 -0500

    modified test.js for assignment 011(variable scope)

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-function getAverage (a,b,c,d,e,f){
+// last part of the vid has him putting the var average outside of the function but I am not going to do that since it isn't used elsewhere and doesn't need to be a global variable.
 
-    var average = (a + b + c + d + e + f) / 6;
+function getAverage(a, b, c, d, e, f) {
+
+    var average = (a + b + c + d + e + f) / 6; //local variable
     console.log(average);
     return average;
 
 }
 
+var myResult = getAverage(7, 8, 9, 10, 11, 12); //global variable
 
-var myResult = getAverage(7,8,9,10,11,12);
-console.log("the average is " + myResult);
+function logResult() {
+
+    console.log("the average is " + myResult + " inside the function");
+}
+
+logResult();
